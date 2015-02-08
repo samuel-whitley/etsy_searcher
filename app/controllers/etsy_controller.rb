1830484163e9bf2f51ade0f0d80617d21311c247
samuel <samuel.whitley89@gmail.com>
2015-02-08 18:45:40 -0500

    It has some resemblance of a webpage.

---
+++ app/controllers/etsy_controller.rb
@@ -0,0 +1,10 @@
+
+class EtsyController < ApplicationController
+  def search
+    data = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], keywords: params[:q])
+     @response = JSON.parse(data.body)['results']
+    #@response = @response.to_json
+    #@response = JSON.parse(@repsonse)
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/etsy_search', to: 'etsy#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
