040126c32b6d90dc3b5130c3e30730c557117eb6
samuel <samuel.whitley89@gmail.com>
2015-02-08 22:32:43 -0500

    looks more like a webpage, images link to etsy page.

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -1,8 +1,10 @@
 
 class EtsyController < ApplicationController
   def search
+    
     data = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], keywords: params[:q])
      @response = JSON.parse(data.body)['results']
+     binding.pry
     #@response = @response.to_json
     #@response = JSON.parse(@repsonse)
     
